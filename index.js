156ba00a0ff0ca505b16fafbb1768de16e55e38e
tariq1510 <shahtariqmak@gmail.com>
2024-06-20 16:28:49 +0500

    calculator

---
+++ index.js
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+// UPLoad Through Gitup Desktop
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter First Number", type: "number", name: "firstNumber" },
+    { message: "Enter Second Number", type: "number", name: "secondNumber" },
+    {
+        message: "Select any Option Below",
+        type: "list",
+        name: "operator",
+        choices: ["Plus", "Minus", "Multiply", "Divide"],
+    },
+]);
+// conditional statement
+if (answer.operator === "Plus") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "Minus") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "Multiply") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "Divide") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("YOU ARE WRONG!");
+}
